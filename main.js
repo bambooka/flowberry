d1526756032a11b8deff07d9ee58ee2814f0f8a6
OlaBooka <booka@yandex.ru>
2017-09-27 17:45:46 +0300

    Customize slider

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 $('.slider').slick({
     autoplay: true,
     dots: true,
-    arrows: false
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 3000,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '10px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 1500,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '10px',
+                slidesToShow: 1
+            }
+        },
+
+        {
+            breakpoint: 1300,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '10px',
+                slidesToShow: 1
+            }
+        }
+    ]
+
     }
 )
